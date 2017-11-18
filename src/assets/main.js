ea316ac04d2ab6d783160f793059d36db858dc34
imac-01 <tomharrison@Toms-iMac.local>
2017-11-18 15:52:48 +0000

    solved project

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,45 @@
 $(function() {
 
-  // your code will go here
+  $.ajax({
+    url: 'https://www.codeschool.com/users/tjharrison.json',
+    dataType: 'jsonp',
+    success: function (response) {
+      //handle response
+      //console.log('response:', response);
+      addCourses(response.courses.completed)
+    }
+  });
+
+
+  function addCourses(courses) {
+
+    let $badges = $('#badges');
+
+    courses.forEach(function (course) {
+
+      let $course = $('<div />', {
+        'class': 'course'
+      }).appendTo($badges);
+
+      $('<h3 />', {
+        text: course.title
+      }).appendTo($course);
+
+      $('<img />', {
+        src: course.badge
+      }).appendTo($course);
+
+      $('<a />', {
+        'class': 'btn btn-primary',
+        target: '_blank',
+        href: course.url,
+        text: 'See Course'
+      }).appendTo($course);
+
+    });
+  }
+
+
+
 
 });
